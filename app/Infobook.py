6def5346810bf8ead57e477774cff77839d7da9f
doniyor2012py <adoniyor2012@gmail.com>
2026-02-20 18:00:49 +0000

    syntax fix

--- app/Infobook.py
+++ app/Infobook.py
@@ -15,13 +15,14 @@ print("📂 Full path:", os.path.abspath(__file__))
 
 
 def Main(page: ft.Page):
-    analytics = "<!DOCTYPE html>
-<html lang="ru">
+    analytics = "
+    <!DOCTYPE html>
+<html lang='ru'>
 <head>
-  <meta charset="UTF-8">
+  <meta charset='UTF-8'>
   <!-- Privacy-friendly analytics by Plausible -->
 <!-- Privacy-friendly analytics by Plausible -->
-<script async src="https://plausible.io/js/pa-Q_jj32-309OTnnqJrvPZ9.js"></script>
+<script async src='https://plausible.io/js/pa-Q_jj32-309OTnnqJrvPZ9.js'></script>
 <script>
   window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
   plausible.init()
